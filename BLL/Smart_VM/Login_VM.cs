2610e91fcd8490bae5236c9b421a285867718da5
5 <5@DESKTOP-VGBHK07>
2022-01-11 16:01:04 +0200

    ViewModel

---
+++ BLL/Smart_VM/Login_VM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Smart_VM
+{
+    public class Login_VM
+    {
+        public string  UserName { get; set; }
+        public string  UserPassword { get; set; }
+    }
+}
