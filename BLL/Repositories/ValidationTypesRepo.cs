b8033af5076b8753d283f7e9c331a15cfe36d0eb
4stechgit <4STechgit@gmail.com>
2022-02-13 13:58:55 +0200

    add repo for last models (validationTypes,userDisability,controle,reservationForm)

---
+++ BLL/Repositories/ValidationTypesRepo.cs
@@ -0,0 +1,19 @@
+﻿using BLL.IRepositories;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Repositories
+{
+    public class ValidationTypesRepo : BaseRepo<ValidationTypes>, IValidationTypesRepo
+    {
+        private readonly SmartContext _context;
+        public ValidationTypesRepo(SmartContext context) : base(context)
+        {
+            this._Context = context;
+        }
+    }
+}
